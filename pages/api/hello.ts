f8107769a46fefcd434cea5663dee31ba9491a30
ol222hf <ol222hf@student.lnu.se>
2021-12-07 18:24:51 +0100

    Testing fake users rendering using my caching

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import getMembers from "utils/test";
 
-type Data = {
-  name: string
-}
+type Member = {
+  email: String;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+  res: NextApiResponse<Member[]>
+): Promise<void> {
+  res.status(200).json(await getMembers());
 }
---
+++ types/Member.ts
@@ -0,0 +1,5 @@
+type Member = {
+  email: String;
+};
+
+export default Member;
