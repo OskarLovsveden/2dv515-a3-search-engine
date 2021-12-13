87c016fe2c51c32b89b2661f0da0d6b0e13141b2
ol222hf <ol222hf@student.lnu.se>
2021-12-13 18:06:20 +0100

    update initial get method towards api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,24 @@
-import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
+import PageDB from "models/PageDB";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import styles from "styles/Home.module.css";
-import Member from "types/Member";
 
 type HomeProps = {
-  members: Member[];
+  pageDB: PageDB;
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch("http://localhost:3000/api/hello");
-  const members = await res.json();
-  console.log(members);
+  const res = await fetch("http://localhost:3000/api/search");
+  const pageDB = await res.json();
 
   return {
     props: {
-      members,
+      pageDB,
     },
   };
 };
 
-const Home: NextPage<HomeProps> = ({ members }) => {
+const Home: NextPage<HomeProps> = ({ pageDB }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -28,11 +27,7 @@ const Home: NextPage<HomeProps> = ({ members }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        {members.map((member: Member, index: number) => (
-          <p key={index}>{member.email}</p>
-        ))}
-      </main>
+      <main className={styles.main}>MAIN</main>
     </div>
   );
 };
