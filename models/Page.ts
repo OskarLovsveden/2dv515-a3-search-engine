6bdcbda278556ba7b2a8de193aa1d037f630f793
ol222hf <ol222hf@student.lnu.se>
2021-12-13 18:05:32 +0100

    Page class

---
+++ models/Page.ts
@@ -0,0 +1,35 @@
+type PageJson = {
+  url: string;
+  size: number;
+};
+
+class Page {
+  private _url: string;
+  private _words: number[];
+
+  constructor(url: string) {
+    this._url = url;
+    this._words = [];
+  }
+
+  get url(): string {
+    return this._url;
+  }
+
+  get words(): number[] {
+    return this._words;
+  }
+
+  addWordId(id: number) {
+    this._words.push(id);
+  }
+
+  toJSON(): PageJson {
+    return {
+      url: this._url,
+      size: this._words.length,
+    };
+  }
+}
+
+export default Page;
