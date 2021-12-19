9627011ec73e70110e5a618976a0e5c87d623429
ol222hf <ol222hf@student.lnu.se>
2021-12-19 23:20:52 +0100

    swap pages, present number of results

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,35 @@
-import PageDB from "models/PageDB";
-import type { GetStaticProps, NextPage } from "next";
+import type { NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
+
 import styles from "styles/Home.module.css";
 
-type HomeProps = {
-  pageDB: PageDB;
-};
+import QueryResTable from "components/QueryResTable";
+import Searchbar from "components/Searchbar";
+import { Score } from "types/Score";
 
-export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch("http://localhost:3000/api/search");
-  const pageDB = await res.json();
+const Home: NextPage = () => {
+  const [queryTime, setQueryTime] = useState<number>(0);
+  const [queryRes, setQueryRes] = useState<Array<Score>>(new Array<Score>());
+  const [pagination, setPagination] = useState<number>(5);
 
-  return {
-    props: {
-      pageDB,
-    },
+  const onFirstPage = pagination === 5;
+  const onLastPage = pagination + 5 > queryRes.length;
+  const currentPage = pagination / 5;
+  const lastPage = Math.round(queryRes.length / 5);
+  const pageNumbers = "page " + currentPage + " of " + lastPage;
+  const numOfResultsAndTime =
+    "Found " + queryRes.length + " results in " + queryTime.toFixed(3) + " sec";
+
+  const resultHandler = (data: Array<Score>, time: number) => {
+    setQueryRes(data);
+    setQueryTime(time);
+    setPagination(5);
   };
-};
 
-const Home: NextPage<HomeProps> = ({ pageDB }) => {
+  const pageBack = () => setPagination(pagination - 5);
+  const pageForward = () => setPagination(pagination + 5);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -27,7 +38,28 @@ const Home: NextPage<HomeProps> = ({ pageDB }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>MAIN</main>
+      <main className={styles.main}>
+        <Searchbar resultHandler={resultHandler} />
+        {queryRes.length !== 0 && (
+          <div>
+            <div>
+              <QueryResTable
+                queryRes={queryRes}
+                startAt={pagination - 5}
+                endAt={pagination}
+              />
+              <button disabled={onFirstPage} onClick={pageBack}>
+                {"<"}
+              </button>
+              <span className="mx-1">{pageNumbers}</span>
+              <button disabled={onLastPage} onClick={pageForward}>
+                {">"}
+              </button>
+            </div>
+            <div>{numOfResultsAndTime}</div>
+          </div>
+        )}
+      </main>
     </div>
   );
 };
