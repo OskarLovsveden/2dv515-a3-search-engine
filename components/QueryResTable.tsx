e6c3a7e07c89e2b678d61a8cdaeed1cb7ad0ff1f
ol222hf <ol222hf@student.lnu.se>
2021-12-19 22:57:28 +0100

    render query result as table

---
+++ components/QueryResTable.tsx
@@ -0,0 +1,42 @@
+import styles from "styles/QueryResTable.module.css";
+import { Score } from "types/Score";
+import { round } from "utils/helpers";
+
+type QueryResTableProps = {
+  queryRes: Score[];
+  startAt: number;
+  endAt: number;
+};
+
+const QueryResTable = ({ queryRes, startAt, endAt }: QueryResTableProps) => {
+  return (
+    <table className={styles.table}>
+      <tr>
+        <th>Link</th>
+        <th>Score</th>
+        <th>Content</th>
+        <th>Location</th>
+        <th>PageRank</th>
+      </tr>
+      {queryRes.slice(startAt, endAt).map((q: Score, i: number) => (
+        <tr key={i}>
+          <td>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={`https://en.wikipedia.org/wiki/${q.url}`}
+            >
+              {q.url}
+            </a>
+          </td>
+          <td>{round(q.score)}</td>
+          <td>{round(q.content)}</td>
+          <td>{round(q.location)}</td>
+          <td>0</td>
+        </tr>
+      ))}
+    </table>
+  );
+};
+
+export default QueryResTable;
